c5c1646a605d26bb2e39888d19c50493802b21e0
LizVValdez <liza.valdez@outlook.com>
2022-07-26 10:49:49 -0300

    Modificación de los CROS

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-java -Dserver.port=$PORT $JAVA_OPTS -jar target/YoProgramoValdez-0.0.1-SNAPSHOT.jar
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/YoProgramoValdez-0.0.1-SNAPSHOT.jar
 
--- src/main/java/com/Portfolio/YoProgramoValdez/security/WebSecurity.java
+++ src/main/java/com/Portfolio/YoProgramoValdez/security/WebSecurity.java
@@ -58,8 +58,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**","/","/**").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
                 .and()
